b9e49548f5bb5dcd534a2c6b243a857cf0be6c2e
HAYASAKA.Ryosuke <hayasaka.ryosuke@gmail.com>
2014-06-22 21:16:55 +0900

    addtest

--- zamza/DrawGraphics.py
+++ zamza/DrawGraphics.py
@@ -1,16 +1,27 @@
 #!coding:utf-8
-from PIL import ImageDraw, Image
+from PIL import ImageDraw, Image, ImageFont
 
 
-class ICdraw(object):
+class ICdraw:
     def __init__(self):
-        self.im = Image.new('RGB', (100, 100), 'rgb(128,128,128)')
-        self.draw = ImageDraw.Draw(self.im)
+        self.im = Image.new('RGBA', (480, 780), (128,128,128,0))
+        self.drawline = ImageDraw.Draw(self.im)
+        self.font = ImageFont.truetype(size=12)
 
-    def drawIC(self, *args):
-        self.line(((0, 0), (50, 50), (80, 20)), fill='rgb(255, 0, 0)')
+    def contour(self,x,y,width,height,color='black'):
+        self.drawline.line(((x, y), (x, y+height), (x+width, y+height),(x+width, y),(x, y)), fill=color,width=2)
 
+    def pin(self):
+        self.drawline.text((5,5),"hello")
+
+    def draw(self,one,two,three,four):
+        self.pin()
+        #self.contor()
+
+    def show(self):
+        self.im.show()
 
 class Import(object):
     def __init__(self):
         pass
+
