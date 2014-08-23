6f2a8131f19696bd08e3ba0314611397b43eac44
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-08-24 01:10:49 +0900

    add Fonts

--- zamza/DrawGraphics.py
+++ zamza/DrawGraphics.py
@@ -14,7 +14,7 @@ class ICdraw:
     def __init__(self):
         self.im = Image.new('RGBA', (480, 780), (128, 128, 128, 0))
         self.drawline = ImageDraw.Draw(self.im)
-        self.font = ImageFont.truetype(os.path.dirname(__file__) + "/../font/RictyDiminishedDiscord-Regular.ttf", size=16)
+        self.font = ImageFont.truetype(os.path.dirname(__file__) + "/../font/ipaexg.ttf", size=16)
         self.widthmargin = self.leftmargin + self.rightmargin
         self.heightmargin = self.topmargin + self.bottommargin
 
