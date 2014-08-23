6d38110b305b5ea8431676209275f9bece348768
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-08-24 00:23:20 +0900

    ICDraw

--- tests/DrawGraphics_test.py
+++ tests/DrawGraphics_test.py
@@ -1,18 +1,56 @@
-#!coding:utf-8
+# coding:utf-8
 
 import sys
 sys.path.append('zamza')
 
 import DrawGraphics
+import unittest
+
+#x = 3
+#y = 3
+#width = 100
+#height = 100
+#ic = DrawGraphics.ICdraw()
+##ic.draw(x,y,width,height)
+#ic.pin()
+#ic.contour(x, y, width, height)
+#
+#ic.show()
+
 
 x = 3
 y = 3
-width = 100
-height = 100
 ic = DrawGraphics.ICdraw()
-#ic.draw(x,y,width,height)
-ic.pin()
-ic.contour(x,y,width,height)
-
+ic.pin(top=0, left=8, bottom=0, right=8)
+ic.text(top=[],
+        left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+        bottom=[],
+        right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
+        )
+ic.draw(x, y)
 ic.show()
-
+x = 3
+y = 3
+ic = DrawGraphics.ICdraw()
+ic.pin(top=8, left=8, bottom=8, right=8)
+ic.text(top=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+        left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+        bottom=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+        right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
+        )
+ic.draw(x, y)
+ic.show()
+#class testDrawGraphics(unittest.TestCase):
+#    def drawic(self):
+#        x = 3
+#        y = 3
+#        ic = DrawGraphics.ICdraw()
+#        ic.pin(top=0, left=8, bottom=0, right=8)
+#        ic.text(top=[],
+#                left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+#                bottom=[],
+#                right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
+#                )
+#        ic.draw(x, y)
+#        ic.show()
+#unittest.main()
--- zamza/DrawGraphics.py
+++ zamza/DrawGraphics.py
@@ -1,27 +1,53 @@
 #!coding:utf-8
 from PIL import ImageDraw, Image, ImageFont
+import os
 
 
 class ICdraw:
+    topmargin = 20
+    bottommargin = 20
+    leftmargin = 40
+    rightmargin = 40
+    pinmargin = 10
+    fontsize = 16
+
     def __init__(self):
-        self.im = Image.new('RGBA', (480, 780), (128,128,128,0))
+        self.im = Image.new('RGBA', (480, 780), (128, 128, 128, 0))
         self.drawline = ImageDraw.Draw(self.im)
-        self.font = ImageFont.truetype(size=12)
+        self.font = ImageFont.truetype(os.path.dirname(__file__) + "/../font/RictyDiminishedDiscord-Regular.ttf", size=16)
+        self.widthmargin = self.leftmargin + self.rightmargin
+        self.heightmargin = self.topmargin + self.bottommargin
 
-    def contour(self,x,y,width,height,color='black'):
-        self.drawline.line(((x, y), (x, y+height), (x+width, y+height),(x+width, y),(x, y)), fill=color,width=2)
+    def contour(self, x, y, width, height, color='black'):
+        self.drawline.line(((x, y), (x, y+height), (x+width, y+height), (x+width, y), (x, y)), fill=color, width=2)
 
-    def pin(self):
-        self.drawline.text((5,5),"hello")
+    def pin(self, top=0, left=0, bottom=0, right=0):
+        self.width = max(top, bottom) * self.pinmargin + max(top, bottom) * self.fontsize + self.widthmargin
+        self.height = max(left, right) * self.pinmargin + max(left, right) * self.fontsize + self.heightmargin
 
-    def draw(self,one,two,three,four):
-        self.pin()
-        #self.contor()
+    def draw(self, x, y):
+        fontsize = self.fontsize
+        self.contour(x=x, y=y, width=self.width, height=self.height)
+        for i, text in enumerate(self.topictext):
+            self.drawline.text((x+(i*self.pinmargin)+(i*fontsize)+self.widthmargin//2, y), str(text), font=self.font)
+        for i, text in enumerate(self.leftictext):
+            self.drawline.text((x, y+(i*self.pinmargin)+(i*fontsize)+self.heightmargin//2), str(text), font=self.font)
+        for i, text in enumerate(self.bottomictext):
+            self.drawline.text((x+(i*self.pinmargin)+(i*fontsize)+self.widthmargin//2, y+self.height-self.bottommargin//2), str(text), font=self.font)
+        for i, text in enumerate(self.rightictext):
+            self.drawline.text((x+self.width-self.rightmargin//2, y+(i*self.pinmargin)+(i*fontsize)+self.heightmargin//2), str(text), font=self.font)
+
+    def text(self, top, left, bottom, right):
+        self.topictext = top
+        self.leftictext = left
+        self.bottomictext = bottom
+        self.rightictext = right
+
 
     def show(self):
         self.im.show()
 
+
 class Import(object):
     def __init__(self):
         pass
-
