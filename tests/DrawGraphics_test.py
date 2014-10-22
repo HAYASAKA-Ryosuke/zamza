437f3432719233e8b6ca7025ae66b5da5d2322f0
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-10-22 21:11:37 +0900

    change drawictest

--- tests/DrawGraphics_test.py
+++ tests/DrawGraphics_test.py
@@ -37,15 +37,18 @@ class testDrawGraphics(unittest.TestCase):
         y = 300
         self.ic.pin(top=8, left=8, bottom=8, right=8)
         self.ic.text(top=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-                left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-                bottom=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-                right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
+                left=["p9", "p10", "p12", "p13", "p14", "p15", "p16", "p17"],
+                bottom=["p18", "p19", "p20", "p21", "p22", "p23", "p24", "p25"],
+                right=["p26", "p27", "p28", "p29", "p30", "p31", "p32", "p33"]
                 )
         self.ic.name("IC2")
         self.ic.draw(x, y)
         self.ic.show()
 
     def test_getpinpos(self):
-        self.assertEqual(("right", 6), self.ic.getpinpos("IC2", "p15"))
+        self.assertEqual(("top", 1), self.ic.getpinpos("IC2", "p2"))
+        self.assertEqual(("left", 2), self.ic.getpinpos("IC2", "p12"))
+        self.assertEqual(("right", 3), self.ic.getpinpos("IC2", "p29"))
+        self.assertEqual(("bottom", 4), self.ic.getpinpos("IC2", "p22"))
 
 unittest.main()
