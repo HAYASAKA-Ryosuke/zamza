4609416d860620a075d7afa75e9f53b782a06459
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-10-22 21:04:51 +0900

    add getpinpos

--- tests/DrawGraphics_test.py
+++ tests/DrawGraphics_test.py
@@ -18,39 +18,34 @@ import unittest
 #ic.show()
 
 
-x = 3
-y = 3
-ic = DrawGraphics.ICdraw()
-ic.pin(top=0, left=8, bottom=0, right=8)
-ic.text(top=[],
-        left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-        bottom=[],
-        right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
-        )
-ic.draw(x, y)
-ic.show()
-x = 3
-y = 3
-ic = DrawGraphics.ICdraw()
-ic.pin(top=8, left=8, bottom=8, right=8)
-ic.text(top=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-        left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-        bottom=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-        right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
-        )
-ic.draw(x, y)
-ic.show()
-#class testDrawGraphics(unittest.TestCase):
-#    def drawic(self):
-#        x = 3
-#        y = 3
-#        ic = DrawGraphics.ICdraw()
-#        ic.pin(top=0, left=8, bottom=0, right=8)
-#        ic.text(top=[],
-#                left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-#                bottom=[],
-#                right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
-#                )
-#        ic.draw(x, y)
-#        ic.show()
-#unittest.main()
+
+class testDrawGraphics(unittest.TestCase):
+    def setUp(self):
+        x = 10
+        y = 10
+        self.ic = DrawGraphics.ICdraw()
+        self.ic.pin(top=0, left=8, bottom=0, right=8)
+        self.ic.text(top=[],
+                left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+                bottom=[],
+                right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
+                )
+        self.ic.name("IC1")
+        self.ic.draw(x, y)
+
+        x = 300
+        y = 300
+        self.ic.pin(top=8, left=8, bottom=8, right=8)
+        self.ic.text(top=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+                left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+                bottom=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+                right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
+                )
+        self.ic.name("IC2")
+        self.ic.draw(x, y)
+        self.ic.show()
+
+    def test_getpinpos(self):
+        self.assertEqual(("right", 6), self.ic.getpinpos("IC2", "p15"))
+
+unittest.main()
--- zamza/DrawGraphics.py
+++ zamza/DrawGraphics.py
@@ -13,12 +13,16 @@ class ICdraw:
 
     def __init__(self):
         self.im = Image.new('RGBA', (480, 780), (128, 128, 128, 0))
+        self.icname = {}
         self.drawline = ImageDraw.Draw(self.im)
         self.font = ImageFont.truetype(os.path.dirname(__file__) + "/../font/ipaexg.ttf", size=16)
         self.widthmargin = self.leftmargin + self.rightmargin
         self.heightmargin = self.topmargin + self.bottommargin
 
-    def contour(self, x, y, width, height, color='black'):
+    def name(self, name):
+        self.icname[name] = {"width": self.width, "height": self.height, "toppin": self.topictext, "leftpin": self.leftictext, "rightpin": self.rightictext, "bottompin": self.bottomictext}
+
+    def __contour(self, x, y, width, height, color='black'):
         self.drawline.line(((x, y), (x, y+height), (x+width, y+height), (x+width, y), (x, y)), fill=color, width=2)
 
     def pin(self, top=0, left=0, bottom=0, right=0):
@@ -27,7 +31,7 @@ class ICdraw:
 
     def draw(self, x, y):
         fontsize = self.fontsize
-        self.contour(x=x, y=y, width=self.width, height=self.height)
+        self.__contour(x=x, y=y, width=self.width, height=self.height)
         for i, text in enumerate(self.topictext):
             self.drawline.text((x+(i*self.pinmargin)+(i*fontsize)+self.widthmargin//2, y), str(text), font=self.font)
         for i, text in enumerate(self.leftictext):
@@ -37,12 +41,29 @@ class ICdraw:
         for i, text in enumerate(self.rightictext):
             self.drawline.text((x+self.width-self.rightmargin//2, y+(i*self.pinmargin)+(i*fontsize)+self.heightmargin//2), str(text), font=self.font)
 
+    def getpinpos(self, icname, pinname):
+        if pinname in self.icname[icname]["toppin"]:
+            for i, name in enumerate(self.icname[icname]["toppin"]):
+                if pinname == name:
+                    return ("top", i)
+        if pinname in self.icname[icname]["leftpin"]:
+            for i, name in enumerate(self.icname[icname]["leftpin"]):
+                if pinname == name:
+                    return ("left", i)
+        if pinname in self.icname[icname]["bottompin"]:
+            for i, name in enumerate(self.icname[icname]["bottompin"]):
+                if pinname == name:
+                    return ("bottom", i)
+        if pinname in self.icname[icname]["rightpin"]:
+            for i, name in enumerate(self.icname[icname]["rightpin"]):
+                if pinname == name:
+                    return ("right", i)
+
     def text(self, top, left, bottom, right):
         self.topictext = top
         self.leftictext = left
         self.bottomictext = bottom
         self.rightictext = right
-
 
     def show(self):
         self.im.show()
