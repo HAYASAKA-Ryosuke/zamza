463a58239a84d650a730506b34c635f19a3abbfd
HAYASAKA.Ryosuke <hayasaka.ryosuke@gmail.com>
2014-11-02 16:55:08 +0900

    refactoring

--- zamza/LineDraw.py
+++ zamza/LineDraw.py
@@ -10,34 +10,24 @@ class LineDraw(object):
         self.stopx = None
         self.stopy = None
 
+    def _gridnum(self,xsmall,ysmall,xlarge,ylarge):
+        res = []
+        for i in range(0,self.__gridseparatenum(ylarge-ysmall)+1):
+            x = []
+            for j in range(0,self.__gridseparatenum(xlarge-xsmall)+1):
+                x.append((xsmall+(self.space * j), ysmall+(self.space * i),))
+            res.append(x)
+        return res
 
     def gridnumlist(self, start=[], stop=[]):
-        res = []
         if start[0] <= stop[0] and start[1] < stop[1]:
-                for i in range(0,self.__gridseparatenum(stop[1]-start[1])+1):
-                    x = []
-                    for j in range(0,self.__gridseparatenum(stop[0]-start[0])+1):
-                        x.append((start[0]+(self.space * j), start[1]+(self.space * i),))
-                    res.append(x)
+            return self._gridnum(start[0], start[1], stop[0], stop[1])
         elif start[0] >= stop[0] and start[1] < stop[1]:
-                for i in range(0,self.__gridseparatenum(stop[1]-start[1])+1):
-                    x = []
-                    for j in range(0,self.__gridseparatenum(start[0]-stop[0])+1):
-                        x.append((stop[0]+(self.space * j), start[1]+(self.space * i),))
-                    res.append(x)
+            return self._gridnum(stop[0], start[1], start[0], stop[1])
         elif start[0] <= stop[0] and start[1] > stop[1]:
-                for i in range(0,self.__gridseparatenum(start[1]-stop[1])+1):
-                    x = []
-                    for j in range(0,self.__gridseparatenum(stop[0]-start[0])+1):
-                        x.append((start[0]+(self.space * j), stop[1]+(self.space * i),))
-                    res.append(x)
+            return self._gridnum(start[0], stop[1], stop[0], start[1])
         elif start[0] >= stop[0] and start[1] > stop[1]:
-                for i in range(0,self.__gridseparatenum(start[1]-stop[1])+1):
-                    x = []
-                    for j in range(0,self.__gridseparatenum(start[0]-stop[0])+1):
-                        x.append((stop[0]+(self.space * j), stop[1]+(self.space * i),))
-                    res.append(x)
-        return res
+            return self._gridnum(stop[0], stop[1], start[0], start[1])
 
     def __gridseparatenum(self, pixel):
         return round(pixel/self.space)
