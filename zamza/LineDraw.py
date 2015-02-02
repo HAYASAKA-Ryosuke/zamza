c9f5a4256f92cb6efb14b86ce7cdbe300744424b
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2015-02-03 01:14:32 +0900

    refactoring

--- zamza/LineDraw.py
+++ zamza/LineDraw.py
@@ -17,35 +17,45 @@ class Linedraw(object):
     def linedraw(self, linelist, linecolor='black'):
         self.drawline.line(linelist, fill=linecolor)
 
+    def _add_edge(self, gridlist, i, j, com):
+        if com == 'y_up':
+            self.graph.add_edge(gridlist[i][j], gridlist[i][j - 1], weight=1)
+        if com == 'y_down':
+            self.graph.add_edge(gridlist[i][j], gridlist[i][j + 1], weight=1)
+        if com == 'x_up':
+            self.graph.add_edge(gridlist[i][j], gridlist[i + 1][j], weight=1)
+        if com == 'x_down':
+            self.graph.add_edge(gridlist[i][j], gridlist[i - 1][j], weight=1)
+
+    def _grid_if(self, gridlist, i, j, xlast, ylast):
+        self.graph.add_node(gridlist[i][j])
+        if i == 0:
+            if j < xlast:
+                self._add_edge(gridlist, i, j, 'y_down')
+            self._add_edge(gridlist, i, j, 'x_up')
+        elif i == ylast:
+            if j < xlast:
+                self._add_edge(gridlist, i, j, 'y_down')
+        else:
+            if i < ylast:
+                self._add_edge(gridlist, i, j, 'x_up')
+            self._add_edge(gridlist, i, j, 'x_down')
+            if j < xlast:
+                self._add_edge(gridlist, i, j, 'y_down')
+            self._add_edge(gridlist, i, j, 'y_up')
+
     def gridgraph(self, start=[], stop=[]):
         gridlist = self.gridnumlist(start, stop)
-        first = 0
         xlast = len(gridlist[0]) - 1
         ylast = len(gridlist) - 1
-        for i in range(0, len(gridlist)):
-            for j in range(0, len(gridlist[0])):
-                self.graph.add_node(gridlist[i][j])
-                if i == first:
-                    if j < xlast:
-                        self.graph.add_edge(gridlist[i][j], gridlist[i][j + 1], weight=1)
-                    self.graph.add_edge(gridlist[i][j], gridlist[i + 1][j], weight=1)
-                elif i == ylast:
-                    if j < xlast:
-                        self.graph.add_edge(gridlist[i][j], gridlist[i][j + 1], weight=1)
-                else:
-                    if i < ylast:
-                        self.graph.add_edge(gridlist[i][j], gridlist[i + 1][j], weight=1)
-                    self.graph.add_edge(gridlist[i][j], gridlist[i - 1][j], weight=1)
-                    if j < xlast:
-                        self.graph.add_edge(gridlist[i][j], gridlist[i][j + 1], weight=1)
-                    self.graph.add_edge(gridlist[i][j], gridlist[i][j - 1], weight=1)
+        # for i in range(ylast + 1):
+        #     for j in range(xlast + 1):
+        [self._grid_if(gridlist, i, j, xlast, ylast) for j in range(xlast + 1) for i in range(ylast + 1)]
         return [p for p in self.nx.all_shortest_paths(self.graph, tuple(start), tuple(stop))]
 
     def _gridnum(self, xsmall, ysmall, xlarge, ylarge):
-
         pos = lambda x, y: (xsmall + (self.space * x), ysmall + (self.space * y),)
         interval_space = lambda pixel: round(pixel / self.space) + 1
-
         yseparate = interval_space(ylarge - ysmall)
         xseparate = interval_space(xlarge - xsmall)
         return [[pos(j, i) for j in range(xseparate)] for i in range(yseparate)]
