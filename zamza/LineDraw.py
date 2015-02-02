bc00511bb9421722436794d576e31706605d5763
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2015-02-03 00:31:18 +0900

    gotyagotya

--- zamza/LineDraw.py
+++ zamza/LineDraw.py
@@ -1,6 +1,7 @@
 #!coding:utf-8
 from PIL import ImageDraw, Image, ImageFont
 import networkx
+from itertools import repeat
 
 
 class Linedraw(object):
@@ -14,20 +15,20 @@ class Linedraw(object):
         self.graph = self.nx.DiGraph()
 
     def linedraw(self, linelist, linecolor='black'):
-        self.drawline.line(linelist,fill=linecolor)
+        self.drawline.line(linelist, fill=linecolor)
 
     def gridgraph(self, start=[], stop=[]):
-        gridlist = self.gridnumlist(start,stop)
+        gridlist = self.gridnumlist(start, stop)
         first = 0
-        xlast = len(gridlist[0])-1
-        ylast = len(gridlist)-1
-        for i in range(0,len(gridlist)):
-            for j in range(0,len(gridlist[0])):
+        xlast = len(gridlist[0]) - 1
+        ylast = len(gridlist) - 1
+        for i in range(0, len(gridlist)):
+            for j in range(0, len(gridlist[0])):
                 self.graph.add_node(gridlist[i][j])
                 if i == first:
                     if j < xlast:
-                        self.graph.add_edge(gridlist[i][j], gridlist[i][j+1],weight=1)
-                    self.graph.add_edge(gridlist[i][j], gridlist[i+1][j],weight=1)
+                        self.graph.add_edge(gridlist[i][j], gridlist[i][j + 1], weight=1)
+                    self.graph.add_edge(gridlist[i][j], gridlist[i + 1][j], weight=1)
                 elif i == ylast:
                     if j < xlast:
                         self.graph.add_edge(gridlist[i][j], gridlist[i][j+1],weight=1)
@@ -40,14 +41,17 @@ class Linedraw(object):
                     self.graph.add_edge(gridlist[i][j], gridlist[i][j-1],weight=1)
         return [p for p in self.nx.all_shortest_paths(self.graph,tuple(start),tuple(stop))]
 
-    def _gridnum(self,xsmall,ysmall,xlarge,ylarge):
-        res = []
-        for i in range(0,self.__gridseparatenum(ylarge-ysmall)+1):
-            x = []
-            for j in range(0,self.__gridseparatenum(xlarge-xsmall)+1):
-                x.append((xsmall+(self.space * j), ysmall+(self.space * i),))
-            res.append(x)
-        return res
+    def _gridnum(self, xsmall, ysmall, xlarge, ylarge):
+
+        def create_pos(x, y):
+            return (xsmall + (self.space * x), ysmall + (self.space * y),)
+
+        def gridseparatenum(pixel):
+            """gridをどの間隔で空けるか"""
+            return round(pixel / self.space) + 1
+        yseparate = gridseparatenum(ylarge - ysmall)
+        xseparate = gridseparatenum(xlarge - xsmall)
+        return [[create_pos(j, i) for j in range(xseparate)] for i in range(yseparate)]
 
     def gridnumlist(self, start=[], stop=[]):
         if start[0] <= stop[0] and start[1] < stop[1]:
@@ -58,7 +62,3 @@ class Linedraw(object):
             return self._gridnum(start[0], stop[1], stop[0], start[1])
         elif start[0] >= stop[0] and start[1] > stop[1]:
             return self._gridnum(stop[0], stop[1], start[0], start[1])
-
-    def __gridseparatenum(self, pixel):
-        return round(pixel/self.space)
-
