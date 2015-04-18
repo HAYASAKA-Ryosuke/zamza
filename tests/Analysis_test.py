02aa6eb0d4cd47490fd1fa27c41e49acb56ebcc5
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2015-04-18 15:04:25 +0900

    test修正

--- tests/Analysis_test.py
+++ tests/Analysis_test.py
@@ -2,17 +2,17 @@
 import unittest
 import sys
 sys.path.append('zamza')
-import Analysis
+import analysis
 
 
 class Analysistest(unittest.TestCase):
     def setUp(self):
-        self.analysis = Analysis.Analysis()
+        self.analysis = analysis.Analysis()
 
     def testtext(self):
         self.assertEqual(self.analysis.text('import A'), {'type': 'import', 'name': 'A'})
         self.assertEqual(self.analysis.text('ic A:'), {'type': 'ic', 'name': 'A'})
-        self.assertEqual(self.analysis.text('left:'), {'type': 'icleft', 'name': ':'})
+        self.assertEqual(self.analysis.text('left:'), {'type': 'ic_left', 'name': ':'})
         self.assertEqual(self.analysis.text('B'), {'type': 'string', 'name': 'B'})
         self.assertEqual(self.analysis.text('A.B-C.D'), {'type': 'wire', 'list': [{'name': 'A', 'pin': 'B'}, {'name': 'C', 'pin': 'D'}]})
         self.assertEqual(self.analysis.text('end'), {'name': 'end', 'type': 'END'})
@@ -41,7 +41,7 @@ class Analysistest(unittest.TestCase):
         '''
         self.analysis.run(data)
         self.assertEqual(self.analysis.ic_info, [{'right_len': 2, 'left_len': 2, 'bottom_len': 2, 'top_len': 2, 'left': [{'name': 'B', 'type': 'string'}, {'name': 'C', 'type': 'string'}], 'ic_name': 'A', 'bottom': [{'name': 'F', 'type': 'string'}, {'name': 'G', 'type': 'string'}], 'right': [{'name': 'D', 'type': 'string'}, {'name': 'E', 'type': 'string'}], 'top': [{'name': 'H', 'type': 'string'}, {'name': 'I', 'type': 'string'}]}])
-        self.assertEqual(self.analysis.maininfo, [{'list': [{'name': 'A', 'pin': 'B'}, {'name': 'A', 'pin': 'C'}], 'type': 'wire'}, {'list': [{'name': 'A', 'pin': 'D'}, {'name': 'A', 'pin': 'I'}], 'type': 'wire'}])
+        self.assertEqual(self.analysis.main_info, [{'list': [{'name': 'A', 'pin': 'B'}, {'name': 'A', 'pin': 'C'}], 'type': 'wire'}, {'list': [{'name': 'A', 'pin': 'D'}, {'name': 'A', 'pin': 'I'}], 'type': 'wire'}])
 
 
 if __name__ == '__main__':
--- tests/DrawGraphics_test.py
+++ tests/DrawGraphics_test.py
@@ -3,7 +3,7 @@
 import sys
 sys.path.append('zamza')
 
-import DrawGraphics
+import draw_graphics
 import unittest
 
 
@@ -11,40 +11,39 @@ class testDrawGraphics(unittest.TestCase):
     def setUp(self):
         x = 50
         y = 50
-        self.ic = DrawGraphics.draw()
-        self.ic.ICpin(top=0, left=8, bottom=0, right=8)
-        self.ic.ICtext(top=[],
-                left=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
+        self.ic = draw_graphics.Draw()
+        self.ic.pin(top=0, left=8, bottom=0, right=8)
+        self.ic.text(top=[],
+                left=["pa1", "pa2", "pa3", "pa4", "pa5", "pa6", "pa7", "pa8"],
                 bottom=[],
-                right=["p9", "p10", "p11", "p12", "p13", "p14", "p15", "p16"]
+                right=["pa9", "pa10", "pa11", "pa12", "pa13", "pa14", "pa15", "pa16"]
                 )
-        self.ic.ICdraw(x, y)
-        self.ic.ICname("IC1")
+        self.ic.draw(x, y)
+        self.ic.name("IC1")
 
         x = 300
         y = 300
-        self.ic.ICpin(top=8, left=8, bottom=8, right=8)
-        self.ic.ICtext(top=["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"],
-                left=["p9", "p10", "p12", "p13", "p14", "p15", "p16", "p17"],
-                bottom=["p18", "p19", "p20", "p21", "p22", "p23", "p24", "p25"],
-                right=["p26", "p27", "p28", "p29", "p30", "p31", "p32", "p33"]
+        self.ic.pin(top=8, left=8, bottom=8, right=8)
+        self.ic.text(top=["pb1", "pb2", "pb3", "pb4", "pb5", "pb6", "pb7", "pb8"],
+                left=["pb9", "pb10", "pb12", "pb13", "pb14", "pb15", "pb16", "pb17"],
+                bottom=["pb18", "pb19", "pb20", "pb21", "pb22", "pb23", "pb24", "pb25"],
+                right=["pb26", "pb27", "pb28", "pb29", "pb30", "pb31", "pb32", "pb33"]
                 )
-        self.ic.ICdraw(x, y)
-        self.ic.ICname("IC2")
+        self.ic.draw(x, y)
+        self.ic.name("IC2")
 
     def test_getpinpos(self):
-        self.assertEqual(("top", 1, (360, 300)), self.ic.ICgetpinpos("IC2", "p2"))
-        self.assertEqual(("left", 2, (290, 360)), self.ic.ICgetpinpos("IC2", "p12"))
-        self.assertEqual(("right", 7, (550, 470)), self.ic.ICgetpinpos("IC2", "p33"))
-        self.assertEqual(("bottom", 4, (420, 510)), self.ic.ICgetpinpos("IC2", "p22"))
+        self.assertEqual(("top", 1, (360, 300)), self.ic.get_pin_pos("IC2", "pb2"))
+        self.assertEqual(("left", 2, (290, 360)), self.ic.get_pin_pos("IC2", "pb12"))
+        self.assertEqual(("right", 7, (550, 470)), self.ic.get_pin_pos("IC2", "pb33"))
+        self.assertEqual(("bottom", 4, (420, 510)), self.ic.get_pin_pos("IC2", "pb22"))
 
     def test_LineDraw(self):
-        print(self.ic.ICDrawArea("IC2"))
-        self.ic.linedraw(self.ic.gridgraph(self.ic.ICgetpinpos("IC1", "p9")[2], self.ic.ICgetpinpos("IC2", "p2")[2])[0])
-        self.ic.linedraw(self.ic.gridgraph(self.ic.ICgetpinpos("IC1", "p12")[2], self.ic.ICgetpinpos("IC2", "p18")[2])[0])
-        self.ic.linedraw(self.ic.gridgraph(self.ic.ICgetpinpos("IC1", "p15")[2], self.ic.ICgetpinpos("IC2", "p3")[2])[0])
-        self.ic.linedraw(self.ic.gridgraph(self.ic.ICgetpinpos("IC1", "p1")[2], self.ic.ICgetpinpos("IC2", "p33")[2])[0])
-        self.ic.linedraw(self.ic.gridgraph(self.ic.ICgetpinpos("IC1", "p8")[2], self.ic.ICgetpinpos("IC2", "p9")[2])[0])
+        self.ic.line_draw(self.ic.grid_graph(self.ic.get_pin_pos("IC1", "pa9")[2], self.ic.get_pin_pos("IC2", "pb2")[2])[0])
+        self.ic.line_draw(self.ic.grid_graph(self.ic.get_pin_pos("IC1", "pa12")[2], self.ic.get_pin_pos("IC2", "pb18")[2])[0])
+        self.ic.line_draw(self.ic.grid_graph(self.ic.get_pin_pos("IC1", "pa15")[2], self.ic.get_pin_pos("IC2", "pb3")[2])[0])
+        self.ic.line_draw(self.ic.grid_graph(self.ic.get_pin_pos("IC1", "pa1")[2], self.ic.get_pin_pos("IC2", "pb33")[2])[0])
+        self.ic.line_draw(self.ic.grid_graph(self.ic.get_pin_pos("IC1", "pa8")[2], self.ic.get_pin_pos("IC2", "pb9")[2])[0])
         self.ic.show()
 
 unittest.main()
